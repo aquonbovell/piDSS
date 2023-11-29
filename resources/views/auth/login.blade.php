b9c2269f9d854b92a79f4521115b6949c168bdeb
Aquon Bovell <aquon_bovell@outlook.com>
2023-11-29 16:47:47 -0400

    Update button styles in login and profile forms

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,7 +39,7 @@
                 </a>
             @endif
 
-            <x-primary-button class="ms-3">
+            <x-primary-button class="ms-3 dark:hover:bg-white dark:focus:bg-white">
                 {{ __('Log in') }}
             </x-primary-button>
         </div>
--- resources/views/profile/partials/update-password-form.blade.php
+++ resources/views/profile/partials/update-password-form.blade.php
@@ -32,7 +32,7 @@
         </div>
 
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
+            <x-primary-button class="dark:hover:bg-white dark:focus:bg-white">{{ __('Save') }}</x-primary-button>
 
             @if (session('status') === 'password-updated')
                 <p
