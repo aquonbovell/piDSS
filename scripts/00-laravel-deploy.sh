e7ba6900a930bb67fa8117a04cdbbedb1b96f9f9
Aquon Bovell <aquon_bovell@outlook.com>
2023-12-04 19:41:21 -0400

    Update composer install command

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-# echo "Running composer"
+echo "Running composer"
 # composer global remove hirak/prestissimo
-# composer install --no-dev --working-dir=/var/www/html
+composer install --no-dev --working-dir=/var/www/html
 
 # echo "Caching config..."
 # php artisan config:cache
@@ -12,8 +12,8 @@
 # echo "Running migrations..."
 # php artisan migrate --force
 
-echo "Running keys..."
-php artisan key:generate --show
+# echo "Running keys..."
+# php artisan key:generate --show
 
 # echo "Running seeders..."
 # php artisan migrate:fresh --seed --force
